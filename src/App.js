3ab0360a2008befc4dbcd9421548362926854170
shinhonggyu <zowoz0@naver.com>
2021-01-04 14:37:23 +0900

    use firebase realtime database - sync cards

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Maker from './components/maker/maker';
 import styles from './App.module.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const App = ({ FileInput, authService }) => {
+const App = ({ FileInput, authService, cardRepository }) => {
   return (
     <div className={styles.app}>
       <Router>
@@ -12,7 +12,11 @@ const App = ({ FileInput, authService }) => {
             <Login authService={authService} />
           </Route>
           <Route path="/maker">
-            <Maker FileInput={FileInput} authService={authService} />
+            <Maker
+              FileInput={FileInput}
+              authService={authService}
+              cardRepository={cardRepository}
+            />
           </Route>
         </Switch>
       </Router>
--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthService from './service/auth_service';
 import ImageUploader from './service/image_uploader';
+import CardRepository from './service/card_repository';
 import ImageFileInput from './components/image_file_input/image_file_input';
 
 const authService = new AuthService();
+const cardRepository = new CardRepository();
 const imageUploader = new ImageUploader();
 const FileInput = (props) => (
   <ImageFileInput {...props} imageUploader={imageUploader} />
@@ -15,7 +17,11 @@ const FileInput = (props) => (
 
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService} FileInput={FileInput} />
+    <App
+      authService={authService}
+      FileInput={FileInput}
+      cardRepository={cardRepository}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
